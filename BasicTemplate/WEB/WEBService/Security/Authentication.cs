3abd389b4e6d120f4c1ddf2381e0e1df7882d495
adam <870995395@qq.com>
2019-02-25 15:04:23 +0800

    登录调整

--- BasicTemplate/WEB/WEBService/Security/Authentication.cs
+++ BasicTemplate/WEB/WEBService/Security/Authentication.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common.Misc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,24 +10,154 @@ namespace WEBService.Security
 {
     public class Authentication
     {
-        
+
         /// <summary>
         /// 登录存储客户端的Cookie名称
         /// </summary>
         private const string LoginToken = "LoginSessionID";
 
+        #region 内置函数
+        private static string GetContextKey(object userId)
+        {
+            return $"LoginContext_{userId}";
+        }
+        #endregion
+
+        #region <当前登录用户对象>
+
+        /// <summary>
+        /// 当前登录用户对象
+        /// </summary>
+        internal static Context CacheContext
+        {
+            get
+            {
+                return GetCurrentContext();
+            }
+        }
+
+        private static Context GetCurrentContext()
+        {
+            string loginSessionID = CurrentSessionID;
+            Context context = new Context();
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(loginSessionID))
+                {
+                    //context = CacheUtil.Get<Context>(loginSessionID);
+                    //if (context == null)
+                    //{
+                    //    string listId = CacheHelper.Get<string>(loginSessionID);
+                    //    if (!string.IsNullOrWhiteSpace(listId))
+                    //    {
+                    //        context = CacheHelper.Get<Context>(listId);
+                    //        if (context == null)
+                    //        {
+                    //            RemoveSessionID(loginSessionID, listId);
+                    //            LogsManager.Error(string.Format("Redis缓存数据为空，key：{0}", listId));
+                    //        }
+                    //        else
+                    //        {
+                    //            context.LoginSessionID = loginSessionID;
+                    //            AddSessionID(loginSessionID, context);
+                    //        }
+                    //    }
+                    //    else
+                    //    {
+                    //        RemoveSessionID(loginSessionID, listId);
+                    //    }
+                    //}
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.Error("获取用户缓存信息异常");
+            }
+            if (context == null)
+            {
+                context = new Context();
+            }
+            return context;
+        }
+        #endregion
+
         public static bool Login(Context context, bool isIgnoreValid = false)
         {
-
-            return true;
+            try
+            {
+                string listId = GetContextKey(context.Id);
+                var CacheContext = "";// CacheHelper.Get<Context>(listId);
+                if (CacheContext != null && isIgnoreValid == false)
+                {
+                    //var configRows = CompanyConfigUtil.GetCompanySysConfig(context.CompanyId);
+                    //bool IsValidateUniqueLogin = configRows.GetConfigValue(CompanyConfigKeys.IsValidateUniqueLogin, true);
+                    //if (IsValidateUniqueLogin)
+                    //{
+                    //    CacheHelper.Remove(CacheContext.LoginSessionID);
+                    //    CacheHelper.Remove(listId);
+                    //}
+                }
+                context.LoginStatus = context.Status;
+                context.GuidKey = GuidUtil.GuidKey;
+                //string url = SkinManager.Url.Replace("//www.", "").Replace("//WWW.", "");
+                context.Token = EncryptionHelper.Md5Encryption(context.GuidKey, "");
+                string loginSessionID = EncryptionHelper.Md5Encryption(context.GuidKey, context.Id.ToString(), "");//SessionRedis.SessionID
+                context.LoginSessionID = $"{LoginToken}_{loginSessionID}";
+                CookieHelper.Set(LoginToken, loginSessionID);
+                AddSessionID(context.LoginSessionID, context);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ex.Error(string.Format("用户登录写入COOKIE异常"));
+                return false;
+            }
         }
 
 
         public static void Logout(bool isSelf = false)
         {
+            try
+            {
+                int userID = CacheContext.Id;
+                if (userID > 0)
+                {
+                    var sessionId = CurrentSessionID;
+                    LogsManager.Debug($"自主登出：用户[{userID}]自主登出，CurrentSessionID：{sessionId}");
+                    string listId = GetContextKey(userID);
 
+                    var setId = "";// GetOnlineUserKey(CacheContext.CompanyId, HomeView);
+                    RemoveSessionID(sessionId, listId, setId, userID, isSelf);
+                }
+                //HttpContext.Current.Session.Remove(HighPassword);
+                //ThirdPartyGamLoginOut();
 
+            }
+            catch (Exception ex)
+            {
+                ex.Error("Logout异常");
+            }
+            string[] cookieNames = new string[] { LoginToken, "LastRequestTime" };
+            foreach (string cookieName in cookieNames)
+            {
+                CookieHelper.Remove(cookieName);
+            }
         }
+        private static void AddSessionID(string loginSessionID, Context context)
+        {
+            //UpdateGameOnlineInfo(context);
+
+            string listId = GetContextKey(context.Id);
+            //CacheUtil.Set(loginSessionID, context, RunTimeOut);
+            //CacheUtil.Set(listId, loginSessionID, RunTimeOut);
+            //CacheHelper.Set(loginSessionID, listId, RedisTimeOut);
+            //CacheHelper.Set(listId, context, RedisTimeOut);
+
+            //var setId = GetOnlineUserKey(context.CompanyId, context.HomeView);
+            //var timeStamp = TimeStamp.DateTimeToUnix(DateTime.Now);
+            //CacheHelper.AddItemToSortedSet(setId, context.Id.ToString(), timeStamp);
+        }
+
 
         private static void RemoveSessionID(string loginSessionID, string listId, string setId = null, int userId = 0, bool isSelf = false)
         {
--- BasicTemplate/WEB/WEBService/Security/Context.cs
+++ BasicTemplate/WEB/WEBService/Security/Context.cs
@@ -9,7 +9,83 @@ namespace WEBService.Security
     [Serializable]
     public class Context
     {
+        public int Id { get; set; }
 
+        public string UserName { get; set; }
 
+        public int GradeId { get; set; }
+
+        public string GradeName { get; set; }
+
+        public int CompanyId { get; set; }
+
+        //public Role Role { get; set; }
+
+        public int Status { get; set; }
+
+        public int LoginStatus { get; set; }
+
+        public int AuthorityID { get; set; }
+
+        public int AuthorityGradeID { get; set; }
+
+        public int ParentID { get; set; }
+
+        public string Ip { get; set; }
+        /// <summary>
+        /// 是否代理
+        /// </summary>
+        public bool IsAgent { get; set; }
+
+        /// <summary>
+        /// 是否为API登录
+        /// </summary>
+        public bool IsAPI { get; set; }
+        /// <summary>
+        /// 是否要强制修改密码
+        /// </summary>
+        public bool ForcesChangePWD { get; set; }
+        /// <summary>
+        /// 是否测试账号
+        /// </summary>
+        public bool IsTestAccount { get; set; }
+
+        /// <summary>
+        /// 用户登录的Token
+        /// </summary>
+        public string Token { get; set; }
+        /// <summary>
+        /// 登录时的GUID
+        /// </summary>
+        public string GuidKey { get; set; }
+
+        ///// <summary>
+        ///// 最后请求时间
+        ///// </summary>
+        //public DateTime LastRequestTime { get; set; }
+        /// <summary>
+        /// 登录时间
+        /// </summary>
+        public DateTime? LoginTime { get; set; }
+        /// <summary>
+        /// 是否推广用户
+        /// </summary>
+        public bool IsGeneralize { get; set; }
+        /// <summary>
+        /// 登录SessionID
+        /// </summary>
+        public string LoginSessionID { get; set; }
+        /// <summary>
+        /// 是否公司客服账号
+        /// </summary>
+        public bool IsCustomerServiceAccount { get; set; }
+
+        public string HomeView { get; set; }
+        /// <summary>
+        /// 当前玩的游戏ID
+        /// </summary>
+        public int CurrentPlayGameId { get; set; }
+
+        public int OldPlayGameId { get; set; }
     }
 }
