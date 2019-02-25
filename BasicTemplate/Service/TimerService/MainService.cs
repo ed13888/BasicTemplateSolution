a0058fe5317ebcb1c0fc918de72ebe323089914c
adam <870995395@qq.com>
2019-02-25 18:37:01 +0800

    调整服务

---
+++ BasicTemplate/Service/TimerService/MainService.cs
@@ -0,0 +1,22 @@
+﻿using Common.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerService
+{
+    public class MainService : WCFServiceFactory
+    {
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BasicTemplate/Service/TimerService/Properties/AssemblyInfo.cs
+++ BasicTemplate/Service/TimerService/Properties/AssemblyInfo.cs
@@ -14,13 +14,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-//将 ComVisible 设置为 false 将使此程序集中的类型
-//对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
+// 将 ComVisible 设置为 false 会使此程序集中的类型
+//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
 //请将此类型的 ComVisible 特性设置为 true。
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("22495a28-56e0-4160-ac4b-1aa1589671a8")]
+[assembly: Guid("7af2560f-6e24-4b13-9a97-2b220c93e8a6")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-// 您可以指定所有值，也可以通过使用“*”来使用
+// 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
