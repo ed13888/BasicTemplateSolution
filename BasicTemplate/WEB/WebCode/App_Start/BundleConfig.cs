67f38c877d25fcd50702e43f6f8031ec29026943
adam <870995395@qq.com>
2019-03-06 14:48:18 +0800

    1

--- BasicTemplate/WEB/WebCode/App_Start/BundleConfig.cs
+++ BasicTemplate/WEB/WebCode/App_Start/BundleConfig.cs
@@ -8,24 +8,28 @@ namespace WebCode
         // 有关捆绑的详细信息，请访问 https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            BundleTable.EnableOptimizations = false;
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Plugins/layer/layer.js",
-                      "~/Plugins/layer/mobile/layer.js"));
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/layer").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Plugins/layer/layer.js",
+                      "~/Plugins/layer/mobile/layer.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
 
-            bundles.Add(new StyleBundle("~/Content/layer").Include(
+            bundles.Add(new StyleBundle("~/Plugins/layer/css").Include(
                       "~/Plugins/layer/theme/default/layer.css",
                       "~/Plugins/layer/mobile/need/layer.css"));
+
+
         }
     }
 }
