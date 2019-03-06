2b526ec4411ac6c52b023f033996e64629f4e7e3
adam <870995395@qq.com>
2019-03-06 13:26:10 +0800

    增加前台

--- BasicTemplate/WEB/WebCode/App_Start/BundleConfig.cs
+++ BasicTemplate/WEB/WebCode/App_Start/BundleConfig.cs
@@ -11,20 +11,21 @@ namespace WebCode
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
-            // 生产准备就绪，请使用 https://modernizr.com 上的生成工具仅选择所需的测试。
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Plugins/layer/layer.js",
+                      "~/Plugins/layer/mobile/layer.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/layer").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+
+            bundles.Add(new StyleBundle("~/Content/layer").Include(
+                      "~/Plugins/layer/theme/default/layer.css",
+                      "~/Plugins/layer/mobile/need/layer.css"));
         }
     }
 }
