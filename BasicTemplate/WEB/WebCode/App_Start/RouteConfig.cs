e01db3bd2b57d22ef9841be4bfb082cade78fa32
adam <870995395@qq.com>
2019-03-07 16:00:17 +0800

    调整路由配置 伪静态

--- BasicTemplate/WEB/WebCode/App_Start/RouteConfig.cs
+++ BasicTemplate/WEB/WebCode/App_Start/RouteConfig.cs
@@ -13,6 +13,26 @@ namespace WebCode
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("pc_index", "index.html", new { controller = "Home", action = "Index" });
+
+            routes.MapRoute(
+               "Action1Html", // action伪静态  
+               "{controller}/{action}.html",// 带有参数的 URL  
+               new { controller = "Home", action = "Index", id = UrlParameter.Optional }// 参数默认值  
+            );
+
+            routes.MapRoute(
+               "IDHtml", // id伪静态  
+               "{controller}/{action}/{id}.html",// 带有参数的 URL  
+               new { controller = "Home", action = "Index", id = UrlParameter.Optional }// 参数默认值  
+            );
+
+            routes.MapRoute(
+                "ControllerHtml", // controller伪静态  
+                "{controller}.html/{action}/{id}",// 带有参数的 URL  
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }// 参数默认值  
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ BasicTemplate/WEB/WebCode/Controllers/TemplateController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCode.Controllers
+{
+    [RoutePrefix("Template")]
+    public class TemplateController : Controller
+    {
+        // GET: Template
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Design/{id:int}.html")]
+        public ActionResult Design(int id)
+        {
+            return View();
+        }
+    }
+}
