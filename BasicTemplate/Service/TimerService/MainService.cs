0ba486a5e9495df5ee431fdeaacf413c79a55a1c
adam <870995395@qq.com>
2019-03-18 18:05:43 +0800

    清理服务

--- BasicTemplate/Service/TimerService/MainService.cs
+++ BasicTemplate/Service/TimerService/MainService.cs
@@ -1,9 +1,15 @@
-﻿using Common.Misc;
+﻿using Common.Entity.Business;
+using Common.Misc;
+using FluentScheduler;
+using log4net.Config;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace TimerService
 {
@@ -15,20 +21,55 @@ namespace TimerService
             {
                 lock (lockRoot)
                 {
-                    //GlobalApp.RunStatus = true;
-                    //ThreadPoolManager.SetThreadCount(120, 300);
-                    //GameDataManager.Start();
-                    //CompanyDataManager.Start();
-                    //if (SystemConfig.IsDevelopment)
-                    //{
-                    //    base.CreateHost<IBalanceService, BalanceService>(SystemConfig.BalanceServiceUrl);
-                    //    base.CreateHost<IOrderBalanceService, OrderBalanceService>(SystemConfig.OrderBalanceServiceUrl);
-                    //    base.CreateHost<IAccessMoneyService, AccessMoneyService>(SystemConfig.AccessMoneyServiceUrl);
-                    //}
-                    //WCFRegister.Register<IBalanceService, BalanceService>(WCFServiceEnum.BalanceService);
-                    //WCFRegister.Register<IOrderBalanceService, OrderBalanceService>(WCFServiceEnum.OrderBalanceService);
-                    //WCFRegister.Register<IAccessMoneyService, AccessMoneyService>(WCFServiceEnum.AccessMoneyService);
-                    //TaskService.Instance.Start();
+                    //Thread.Sleep(20000);
+                    #region 加载配置
+                    //BsonSerializer.RegisterSerializationProvider(new CustomSerializationProvider()); //MongoDB配置
+                    var configDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Config");
+                    XmlConfigurator.Configure(new FileInfo(Path.Combine(configDir, "log4net.config")));
+                    //StructureMapHelpers.Initialize(Path.Combine(configDir, "StructureMap.config"));
+                    //CacheHelper.Configure();
+                    #endregion
+
+                    #region 加载任务列表
+                    var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Config\TimingTask.xml");//读取配置文件中的任务
+                    var xmlStr = File.ReadAllText(path);
+                    var xmls = XElement.Parse(xmlStr);
+                    var taskList = (from x in xmls.Elements("Task")
+                                    select new TaskEntity
+                                    {
+                                        TaskTitle = x.Element("TaskTitle").Value,
+                                        TaskFullName = x.Element("TaskFullName").Value,
+                                        TaskIntervals = x.Element("TaskIntervals").Value.Value<int>(),
+                                        TaskStatus = x.Element("TaskStatus").Value.Value<bool>()
+                                    }).ToList();
+                    var timingTask = new TimingTask();
+                    foreach (var task in taskList)
+                    {
+                        if (string.IsNullOrEmpty(task.TaskFullName))
+                        {
+                            continue;
+                        }
+                        if (!task.TaskStatus)
+                        {
+                            LogsManager.Info($"[{task.TaskTitle}]定时任务未启动");
+                            continue;
+                        }
+                        var taskType = Type.GetType(task.TaskFullName);
+                        if (taskType == null)
+                        {
+                            LogsManager.Error($"[{task.TaskTitle}]定时任务获取实例为空");
+                            continue;
+                        }
+                        var taskjob = Activator.CreateInstance(taskType) as IJob;
+                        timingTask.Schedule(() => taskjob.Execute()).ToRunNow().AndEvery(task.TaskIntervals).Seconds();
+                        LogsManager.Info($"[{task.TaskFullName}]定时任务启动成功");
+                    }
+                    JobManager.Initialize(timingTask);
+
+
+                    #endregion
+
+                    LogsManager.Info("------------------------服务启动成功----------------------");
                 }
             }
             catch (Exception ex)
@@ -39,20 +80,19 @@ namespace TimerService
 
         public override void Stop()
         {
-            //try
-            //{
-            //    lock (lockRoot)
-            //    {
-            //        GlobalApp.RunStatus = false;
-            //        base.Close();
-            //        WCFRegister.Close();
-            //        LogsManager.Info("BalanceService服务关闭成功");
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    ex.Error("系统关闭异常");
-            //}
+            try
+            {
+                lock (lockRoot)
+                {
+                    JobManager.StopAndBlock();
+                    base.Close();
+                    LogsManager.Info("------------------------服务关闭成功----------------------");
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.Error("系统关闭异常");
+            }
         }
     }
 }
---
+++ BasicTemplate/Service/TimerService/Task/CleanTask.cs
@@ -0,0 +1,52 @@
+﻿using Common.Entity.Business;
+using Common.Misc;
+using Common.Misc.SQL;
+using FluentScheduler;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimerService.Task
+{
+    public class CleanTask : IJob
+    {
+        private static readonly object _lock = new object();
+        public void Execute()
+        {
+            lock (_lock)
+            {
+                try
+                {
+                    var list = MySqlHelper.Query<CustomerTemplateInfoEntity>("", "select * from Tcustomertemplateinfo where FNeedDelete=true and FPhoto !='' and FExpireDate <now()").ToList();
+                    //var splitExp = new char[] { ';' };
+                    //var imgList = item.FPhoto.Split(splitExp, StringSplitOptions.RemoveEmptyEntries);
+                    var path = ConfigurationManager.AppSettings["path"];
+                    foreach (var item in list)
+                    {
+                        var dirPath = Path.Combine(path, item.FUId);
+                        if (Directory.Exists(dirPath))
+                        {
+                            DirectoryInfo dir = new DirectoryInfo(dirPath);
+                            dir.Delete(true);
+
+                            LogsManager.Info($"[清理上传图片]FUID:{item.FUId}.");
+                            Thread.Sleep(1000);
+                        }
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    ex.Error("清理任务异常");
+                }
+            }
+
+        }
+    }
+}
